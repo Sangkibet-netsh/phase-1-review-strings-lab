4968dfce67d491e1edaec039dcbf1bb2cebb2f6d
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-14 13:28:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Sang';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// let excitedWelcomeMessage = "WELCOME TO FLATBOOK  !";
+// excitedWelcomeMessage = currentUser;
+// let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser.slice(0, 1)}!`
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
